ab79858686ecc219556c8c6e7c3cf2fa7a1a77ca
sohaib023 <sohaib023@gmail.com>
2020-07-17 05:05:04 +0500

    Delete paint_segments.sh

--- paint_segments.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-JAVA_OPTS="-Xms128m -Xmx512m"
-CLASSPATH="jar/gtgui.jar:jar/tablerecognizer.jar:jar/jiu.jar"
-if [ $# != "4" ] ; then
- echo "Usage : ./paint_segments.sh input_image output_image ground_truth_file word_box_file"
-else
- java $JAVA_OPTS -cp $CLASSPATH de.dfki.trecs.groundtruth.image.GroundTruthImage $1 $2 $3 $4
-fi
